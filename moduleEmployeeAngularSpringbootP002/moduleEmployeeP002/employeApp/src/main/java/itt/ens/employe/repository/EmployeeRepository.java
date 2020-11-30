d91c98bb6a7ab789ede83fdc3559389177fcb868
herve boris Mbou lontsi <herveborismboulontsi@gmail.com>
2020-11-30 23:23:12 +0100

    my commit

---
+++ moduleEmployeeAngularSpringbootP002/moduleEmployeeP002/employeApp/src/main/java/itt/ens/employe/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package itt.ens.employe.repository;
+
+import itt.ens.employe.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ moduleEmployeeAngularSpringbootP002/moduleEmployeeP002/employeApp/src/test/java/itt/ens/employe/EmployeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package itt.ens.employe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
